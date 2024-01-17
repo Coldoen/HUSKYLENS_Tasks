f10c23114114a7b889e6db34062a1d76a05e32d3
Coldoen <hadjadj@et.esiea.fr>
2024-01-17 18:46:50 +0100

    Add code ex3 Post-It

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,12 @@ int err_I = 0;
 HUSKYLENS huskylens;
 
 enum etat_e {
-    IDLE,STOP,LABA,DANCE, SEARCH, TURN, CONFIG, READY, APPROACH
+    IDLE,STOP,LABA,DANCE, TURN_LEFT, TURN_RIGHT, GOBACK, READY
     };
 
 etat_e etat;
 unsigned long last_millis = 0;
-int numColor = 0; // Calculer la prochaine couleur attendu
+int numColor = 1; // Calculer la prochaine couleur attendu
 
 //Configuration des moteurs
 void cmd_moteurs(int mG, int mD){
@@ -74,6 +74,16 @@ int isMerc(int id_color1){
   return 0;
 }
 
+void accelerateToBall() {
+  cmd_robot(150, 0);
+}
+
+//Distance à laquel le robot va s'arrêter si il est à moins de 140 pixels de la couleur
+bool arrColor(HUSKYLENSResult color){
+
+  return (color.height >= 140);
+}
+
 float measureDistance() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
@@ -89,16 +99,6 @@ float measureDistance() {
 bool isCloseToBall() {
   float distance = measureDistance();
   return (distance < 30); // Example threshold in centimeters
-}
-
-//Distance à laquel le robot va s'arrêter si il est à moins de 140 pixels de la couleur
-bool arrColor(HUSKYLENSResult color){
-
-  return (color.height >= 100);
-}
-
-void accelerateToBall() {
-  cmd_robot(150, 0);
 }
 
 //GRAPH (Fonction permettant de passer d'un état à l'autre)
@@ -120,12 +120,11 @@ bool dealyPassed(int dealy_ms){
 void printResult(HUSKYLENSResult result);
 
 void setup() {
-    etat = CONFIG;
+    etat = IDLE;
     pinMode(PWMA, OUTPUT);
     pinMode(DIRA, OUTPUT);
     pinMode(PWMB, OUTPUT);
     pinMode(DIRB, OUTPUT);
-    // Ultrasonic sensor setup
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
     Serial.begin(115200);
@@ -139,25 +138,22 @@ void setup() {
     }
 }
 
-HUSKYLENSResult getColor(int id_color){
-
-  HUSKYLENSResult color_return;
-  if(!huskylens.request()){
-
-    color_return.ID = 1;
-    return color_return;
+// Check if the expected color ID is detected
+bool getColor(int expectedId) {
+  if (!huskylens.request()) {
+    return false;
   }
-  while(huskylens.available()){
 
-    color_return = huskylens.read();
-    if(color_return.ID == id_color){
-
-      return color_return;
+  while (huskylens.available()) {
+    HUSKYLENSResult color = huskylens.read();
+    if (color.ID == expectedId) {
+      return true;  // Expected color ID found
     }
   }
-  color_return.ID = 1;
-  return color_return;
+  return false;  // Expected color ID not found
 }
+
+
   //Valeur, formule et calcul des vitesses du robot 
   void assert(HUSKYLENSResult color){
 
@@ -187,12 +183,12 @@ void loop() {
 
   switch (etat) {
 
-    case CONFIG:{
+    // case CONFIG:{
 
-      huskylens.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION);
-      newState(IDLE);
-    }
-    break;
+    //   huskylens.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION);
+    //   newState(IDLE);
+    // }
+    // break;
     
 
     case IDLE:{
@@ -203,50 +199,64 @@ void loop() {
     }
     break;
 
-    case READY: 
+    case READY: {
 
       if(dealyPassed(2000)){
 
         huskylens.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION);
         newState(LABA); //Attend deux secondes puis passe à l'état LABA
       }
+    }
     break;
   
     case LABA: {
-
-      HUSKYLENSResult color = getColor(numColor);
-      if (color.ID != 0) {
-        if (isCloseToBall()) {
-          newState(APPROACH);
-         } else {
-          assert(color); // Continue moving towards the ball
-        }
-      }
-  
-      if (arrColor(color) && numColor == 4) {
-
-        newState(DANCE); //Quand toutes les couleurs ont été apperçu, passe en mode DANCE
-
-      }else if(arrColor(color) && numColor != 4) {
-            
-        numColor ++;
-        newState(SEARCH); 
-      }
-    }
-  break;
-
-    case APPROACH: {
+  if (getColor(numColor)) {
+    // Expected color ID is detected
+    while (getColor(numColor)) {
       accelerateToBall(); // Accelerate towards the ball
-      delay(1200); // Give time for the robot to reach the ball (adjust as needed)
-      numColor ++;
-      if(numColor != 4){
-      newState(SEARCH); // Switch to SEARCH mode after hitting the ball
-      }else{
-        newState(DANCE);
-      }
+    }  
+    numColor++;
+    if (numColor == 5) {
+      newState(DANCE);
+    } else if (numColor == 2) {
+      newState(TURN_RIGHT);
+    } else if (numColor == 3) {
+      newState(TURN_RIGHT);
+    } else if (numColor == 4) {
+      newState(TURN_LEFT);
     }
-  break;
+  }
+}
+break;
 
+    case TURN_LEFT: {
+
+      while(!isMerc(numColor)){
+        cmd_robot(0, -60); 
+      }
+      newState(LABA); //Repasse en mode LABA
+      }
+    break;
+
+    case TURN_RIGHT: {
+
+      while(!isMerc(numColor)){
+        cmd_robot(0, 60); 
+      }
+      newState(LABA); //Repasse en mode LABA
+    }
+    break;
+
+    // case GOBACK: {
+
+    //   while(!isMerc(numColor)){
+    //     cmd_robot(80, 60); 
+    //   }
+    //   newState(LABA); //Repasse en mode LABA
+    //   }
+    // break;
+
+    
     case DANCE:{
 
       cmd_robot(0, 500);
@@ -265,31 +275,6 @@ void loop() {
         newState(DANCE);
       }
     }  
-    break;
-
-    case SEARCH:{
-    
-      cmd_robot(0, 0);
-      if (!isMerc(numColor)) {
-        
-        if(dealyPassed(500)){
-        
-          newState(TURN); //Si aucune couleur n'est détecté, passe en mode TURN
-        }
-      }else{
-
-        newState(LABA); // Retourne en mode LABA lorsque la bonne couleur est détecté
-      }
-    }
-      break;  
-
-    case TURN: {
-
-      cmd_robot(0, -80); 
-      if(dealyPassed(500)){;
-        newState(SEARCH); //Repasse en mode SEARCH
-      }
-    }
     break;
   }
 }
